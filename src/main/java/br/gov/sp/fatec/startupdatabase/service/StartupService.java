1e48515fce014d914a9d751aebe8baf57fba7727
camilobmoreira <camilo.moreira@fatec.sp.gov.br>
2018-11-01 20:59:57 -0200

    Criado testes de exemplo
    Testes usando assertEquals, assertSame, assertThrows, assertAll,
    assumeTrue e fail

--- src/main/java/br/gov/sp/fatec/startupdatabase/service/StartupService.java
+++ src/main/java/br/gov/sp/fatec/startupdatabase/service/StartupService.java
@@ -1,5 +1,28 @@
 package br.gov.sp.fatec.startupdatabase.service;
 
+import br.gov.sp.fatec.startupdatabase.model.Fundador;
+import br.gov.sp.fatec.startupdatabase.model.Startup;
+
 public class StartupService {
 
+    public Startup criaStartup(String nomeFundador, String cpf, String nomeStartup, String cnpj) {
+        if (cpf != null && !cpf.isEmpty()) {
+            Long.parseLong(cpf);
+        }
+
+        Fundador fundador = new Fundador();
+        fundador.setNome(nomeFundador);
+        fundador.setCpf(cpf);
+
+        Startup startup = new Startup();
+        startup.setNome(nomeStartup);
+        startup.setCnpj(cnpj);
+        startup.setFundador(fundador);
+
+        return startup;
+    }
+
+    public Startup buscaPorNomeFundador(String nomeFundador) {
+        return null;
+    }
 }
