1bce4e40290d945e44bbddd3fc2899b769d0a165
camilobmoreira <camilo.moreira@fatec.sp.gov.br>
2018-11-23 20:15:28 -0200

    Limpando classes para usar projeto na apresentação

--- src/main/java/br/gov/sp/fatec/startupdatabase/model/Startup.java
+++ src/main/java/br/gov/sp/fatec/startupdatabase/model/Startup.java
@@ -1,31 +1,5 @@
 package br.gov.sp.fatec.startupdatabase.model;
 
 public class Startup {
-    private String nome;
-    private String cnpj;
-    private Fundador fundador;
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCnpj() {
-        return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public Fundador getFundador() {
-        return fundador;
-    }
-
-    public void setFundador(Fundador fundador) {
-        this.fundador = fundador;
-    }
 }
