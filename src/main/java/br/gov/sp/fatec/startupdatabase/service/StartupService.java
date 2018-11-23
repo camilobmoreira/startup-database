349e84059f4fe471152a115a4cc265cc0090457b
camilobmoreira <camilo.moreira@fatec.sp.gov.br>
2018-11-23 21:06:29 -0200

    limpando classes para apresentacao

--- src/main/java/br/gov/sp/fatec/startupdatabase/service/StartupService.java
+++ src/main/java/br/gov/sp/fatec/startupdatabase/service/StartupService.java
@@ -8,49 +8,5 @@ import java.util.List;
 
 public class StartupService {
 
-    List<Startup> startups = new ArrayList<>();
 
-    public Startup criaStartup(String nomeFundador, String cpf, String nomeStartup, String cnpj) {
-        this.validaCpf(cpf);
-        Fundador fundador = new Fundador();
-        fundador.setNome(nomeFundador);
-        fundador.setCpf(cpf);
-
-        Startup startup = new Startup();
-        startup.setNome(nomeStartup);
-        startup.setCnpj(cnpj);
-        startup.setFundador(fundador);
-
-        this.startups.add(startup);
-
-        return startup;
-    }
-
-    private void validaCpf(String cpf) {
-        startups.forEach(startup -> {
-            if (startup.getFundador().getCpf().equalsIgnoreCase(cpf)) {
-                throw new RuntimeException("Cpf já cadastrado");
-            }
-        });
-    }
-
-    public List<Startup> buscaPorNomeFundador(String nomeFundador) {
-        List<Startup> startupList = new ArrayList<>();
-        startups.forEach(startup -> {
-            if (startup.getFundador().getNome().equalsIgnoreCase(nomeFundador)) {
-                startupList.add(startup);
-            }
-        });
-        return startupList;
-    }
-
-
-    public Startup buscarPorCpfFundador(String cpf) {
-        for (Startup startup : this.startups) {
-            if (startup.getFundador().getCpf().equalsIgnoreCase(cpf)) {
-                return startup;
-            }
-        }
-        return null;
-    }
 }
