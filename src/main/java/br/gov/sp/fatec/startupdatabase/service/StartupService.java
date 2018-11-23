6f51511cadee5025c7436b0e316bbac9cec62d5e
camilobmoreira <camilo.moreira@fatec.sp.gov.br>
2018-11-23 20:41:42 -0200

    Revertendo ultimo commit

--- src/main/java/br/gov/sp/fatec/startupdatabase/service/StartupService.java
+++ src/main/java/br/gov/sp/fatec/startupdatabase/service/StartupService.java
@@ -5,4 +5,24 @@ import br.gov.sp.fatec.startupdatabase.model.Startup;
 
 public class StartupService {
 
+    public Startup criaStartup(String nomeFundador, String cpf, String nomeStartup, String cnpj) {
+        if (cpf != null && !cpf.isEmpty()) {
+            Long.parseLong(cpf);
+        }
+
+        Fundador fundador = new Fundador();
+        fundador.setNome(nomeFundador);
+        fundador.setCpf(cpf);
+
+        Startup startup = new Startup();
+        startup.setNome(nomeStartup);
+        startup.setCnpj(cnpj);
+        startup.setFundador(fundador);
+
+        return startup;
+    }
+
+    public Startup buscaPorNomeFundador(String nomeFundador) {
+        return null;
+    }
 }
